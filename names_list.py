781952fbf7a0a6e5e1f15247a9c73e07bac95fab
mcvan <mcvanhellm@gmail.com>
2022-11-20 16:32:15 +0300

    innition commit

---
+++ names_list.py
@@ -0,0 +1,6 @@
+names = ['Алесь', 'Захар', 'Алексей']
+for name in names:
+    print('Пошёл ты на ' + name)
+cars = ['BMW', 'audi', 'honda', 'tesla']
+for car in cars:
+    print('Я хотел бы купить автомобиль марки ' + car.title())
