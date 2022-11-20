1b49b0ac4f196713eb8fe3c8c8cae9df0c67db80
mcvan <mcvanhellm@gmail.com>
2022-11-20 15:39:56 +0300

    innition commit

---
+++ name_cases.py
@@ -0,0 +1,7 @@
+name = 'eric'
+print("Hello " + name + " ,would you like to learn some Python today?")
+print("Hello " + name.title() + " ,would you like to learn some Python today?")
+print("Hello " + name.upper() + " ,would you like to learn some Python today?")
+names = "albert einstein"
+quote = 'A person who never made a mistake never tried anything new.'
+print(names.title() + ' once said: ' + quote)
